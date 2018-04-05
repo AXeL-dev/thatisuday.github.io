2765dfe412c1919039c40a088d9a10d5cdb54020
Uday Hiwarale <thatisuday@gmail.com>
2018-04-06 02:04:32 +0530

    new changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const gulpCleanCss = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const pug = require('gulp-pug');
 
 gulp.task('sass', function () {
     return gulp
@@ -14,7 +15,25 @@ gulp.task('sass', function () {
 
 gulp.task('html', function () {
     return gulp
-    .src('./src/**/*.html')
+    .src('./src/**/*.pug')
+    .pipe(pug({
+        locals: {
+            repos: [
+                {name: 'ng-dropzone', desc: 'AngularJS directive for Dropzone, an easy to use drag\'n\'drop file upload library.', stars: '91', forks: '24', downloads: '18k'},
+                {name: 'ng-image-gallery', desc: 'Probably the best angular inline and modal image gallery combined.', stars: '65', forks: '51', downloads: '12k'},
+                {name: 'ngx-image-gallery', desc: 'Probably the best Angular 4+ modal and inline image gallery.', stars: '16', forks: '5', downloads: '4k'},
+                {name: 'npm-time-ago', desc: 'Simple time ago function for node that actually works.', stars: '4', forks: '1', downloads: '13k'},
+                {name: 'npm-sharper', desc: 'Simple time ago function for node that actually works.', stars: '14', forks: '1', downloads: '1k'},
+                {name: 'ng-spin', desc: 'Simple  $http interceptor spinner for angular.js.', stars: '9', forks: '1', downloads: '1k'},
+            ],
+            posts:[
+                {name: 'IndexedDB! Your second step towards Progressive Web Apps (PWA)', url: 'https://medium.com/@thatisuday/indexeddb-your-second-step-towards-progressive-web-apps-pwa-dcbcd6cc2076'},
+                {name: 'Service Workers! Your first step towards Progressive Web Apps (PWA)', url: 'https://itnext.io/service-workers-your-first-step-towards-progressive-web-apps-pwa-e4e11d1a2e85'},
+                {name: 'Achieving multi-threading (parallel programming) in JavaScript using Web Workers', url: 'https://medium.com/@thatisuday/achieving-parallelism-in-javascript-using-web-workers-8f921f2d26db'},
+                {name: 'Making CLI app with ease using commander.js and Inquirer.js', url: 'https://itnext.io/making-cli-app-with-ease-using-commander-js-and-inquirer-js-f3bbd52977ac'},
+            ]
+        }
+    }))
     .pipe(gulp.dest('./'));
 });
 
